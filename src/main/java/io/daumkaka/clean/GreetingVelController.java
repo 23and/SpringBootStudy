08c97905c69c593fe59e5407d4120eb489cf0d1e
23and <44448kg@hanmail.net>
2016-01-13 17:36:26 +0900

    Spring Boot 화면 보여주기 - thymeleaf - veloctity

---
+++ src/main/java/io/daumkaka/clean/GreetingVelController.java
@@ -0,0 +1,16 @@
+package io.daumkaka.clean;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingVelController {
+
+    @RequestMapping("greetingVel")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greetingVel";
+    }
+}
